3c4231f37a6f612310c741e05a0729ee32d84d8d
Henry Todd <htodd2001@gmail.com>
2022-12-23 18:17:53 -0500

    fixed issues with switches project name

--- djangopractice/manage.py
+++ djangopractice/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rwleague.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangopractice.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
